e7b086f700150b0af2eca9d1e2ed8b1b47b3fdfa
Andy Wong <wongandy97@gmail.com>
2016-07-01 23:07:55 -0400

    Add the dataSource, delegates for Search Bar and TableView along with connecting it to FriendSearchViewController

--- Makestagram/ViewControllers/FriendSearch/FriendSearchTableViewCell.swift
+++ Makestagram/ViewControllers/FriendSearch/FriendSearchTableViewCell.swift
@@ -12,6 +12,7 @@ class FriendSearchTableViewCell: UITableViewCell {
     
     @IBOutlet weak var usernameLabel: UILabel!
     @IBOutlet weak var followButton: UIButton!
+    
     @IBAction func followButtonTapped(sender: AnyObject) {
     }
     
