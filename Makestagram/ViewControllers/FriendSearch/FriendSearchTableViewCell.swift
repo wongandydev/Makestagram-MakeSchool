bf5584341eb791b2241d847d297a05bb65e3526c
Andy Wong <wongandy97@gmail.com>
2016-07-01 23:03:58 -0400

    Fix the connections of Outlet, Buttons, and Actions on TableViewCell that was previously connected to TableView

---
+++ Makestagram/ViewControllers/FriendSearch/FriendSearchTableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  FriendSearchTableViewCell.swift
+//  Makestagram
+//
+//  Created by Andy Wong on 7/1/16.
+//  Copyright © 2016 Make School. All rights reserved.
+//
+
+import UIKit
+
+class FriendSearchTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var followButton: UIButton!
+    @IBAction func followButtonTapped(sender: AnyObject) {
+    }
+    
+}
